b0cb15678d30ab46ccd908804dcf7e62af772f92
ricardo-vp <ricardo.vacape@gmail.com>
2022-06-12 00:46:22 -0500

    Configurando mongo y pymongo 🤑

--- src/app.py
+++ src/app.py
@@ -1,6 +1,11 @@
+from crypt import methods
 from flask import Flask
+from flask_pymongo import pymongo
 
 app = Flask(__name__)
+app.config['MONGO_URI'] = 'mongodb://localhost:27017/flaskmongodb'
+
+mongo = pymongo(app)
 
 if __name__ == "__main__":
     app.run(debug=True)
